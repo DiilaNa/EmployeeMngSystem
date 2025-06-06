eb1818edacb6c1907fea28e2e75cab2b32289612
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-06 13:31:45 +0530

    SignUp

--- EMSOne/src/main/java/lk/ijse/project/SignUpServlet.java
+++ EMSOne/src/main/java/lk/ijse/project/SignUpServlet.java
@@ -1,18 +1,77 @@
 package lk.ijse.project;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@WebServlet("api/v1/signup")
+@WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
+    @Resource(name = "java:comp/env/jdbc/pool")
+    private DataSource ds;
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            Connection connection = ds.getConnection();
+            ResultSet resultSet = connection.prepareStatement("select * from user").executeQuery();
+
+            List<Map<String, String>> elist = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Map<String, String> event = new HashMap<String, String>();
+                event.put("name", resultSet.getString("name"));
+                event.put("address", resultSet.getString("address")); /*email address*/
+                event.put("password", resultSet.getString("password"));
+                elist.add(event);
+            }
+
+            resp.setContentType("application/json");
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(resp.getWriter(), elist);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*@Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> event = mapper.readValue(req.getInputStream(), Map.class);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeeManagement", "root", "Ijse@1234");
+
+            PreparedStatement stmt = connection.prepareStatement(
+                    "INSERT INTO event (name,email, password) VALUES (?, ?, ?)"
+            );
+            stmt.setString(1, event.get("name"));
+            stmt.setString(2, event.get("email"));
+            stmt.setString(3, event.get("password"));
+
+            int rows = stmt.executeUpdate();
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(), rows);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 }
